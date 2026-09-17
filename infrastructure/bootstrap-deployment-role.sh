eb7b2a4537606665e0739ded1fee0d1df8771f56
Antonio Freire <github@alf1000.uk>
2026-09-17 21:00:18 +0100

    Pass Cloudflare SSM paths to OIDC role bootstrap

--- infrastructure/bootstrap-deployment-role.sh
+++ infrastructure/bootstrap-deployment-role.sh
@@ -5,6 +5,9 @@ AWS_REGION="${AWS_REGION:-eu-west-2}"
 STACK_NAME="${OIDC_STACK_NAME:-nfc-github-actions-deploy-role}"
 ARTIFACT_PREFIX="${S3_PREFIX:-nfc}"
 DEPLOYMENT_ROLE_NAME="${DEPLOYMENT_ROLE_NAME:-GitHubActionsNfcDeployRole}"
+REQUIRED_API_KEY_PARAMETER="${REQUIRED_API_KEY_PARAMETER:-/lambdas/nfc/required-api-key}"
+SLACK_SIGNING_SECRET_PARAMETER="${SLACK_SIGNING_SECRET_PARAMETER:-/lambdas/aws2022-slack-handler/slack-signing-secret}"
+CLOUDFLARE_API_TOKEN_PARAMETER="${CLOUDFLARE_API_TOKEN_PARAMETER:-/nfc/cloudflare/api-token}"
 
 : "${CODE_BUCKET:?CODE_BUCKET is required and must name the existing private deployment-artifact bucket}"
 
@@ -18,7 +21,10 @@ aws cloudformation deploy \
   --parameter-overrides \
     "CodeBucketName=${CODE_BUCKET}" \
     "ArtifactPrefix=${ARTIFACT_PREFIX}" \
-    "DeploymentRoleName=${DEPLOYMENT_ROLE_NAME}"
+    "DeploymentRoleName=${DEPLOYMENT_ROLE_NAME}" \
+    "RequiredApiKeyParameter=${REQUIRED_API_KEY_PARAMETER}" \
+    "SlackSigningSecretParameter=${SLACK_SIGNING_SECRET_PARAMETER}" \
+    "CloudflareApiTokenParameter=${CLOUDFLARE_API_TOKEN_PARAMETER}"
 
 aws cloudformation describe-stacks \
   --stack-name "${STACK_NAME}" \
